7a20d9841c90991e84a06863f3752d78e29ff3f6
Paul Zadorozhniy <paulzadorozhniy@gmail.com>
2015-11-16 21:43:49 +0200

    created ObiWanLocation view

---
+++ public/views/obiWanLocation.js
@@ -0,0 +1,24 @@
+
+var ObiWanLocation = Backbone.View.extend({
+  el: $(".obi-location"),
+  template: _.template($('#obiLocation').html()),
+
+  initialize: function() {
+    this.model = new ObiLocation();
+    var self = this;
+    var socket = new WebSocket('ws://jedi.smartjs.academy');
+
+    socket.onmessage = function(event) {
+      self.model.setLocation(JSON.parse(event.data).name);
+      self.render();
+    };
+
+    this.render();
+  },
+
+  render: function () {
+    $(this.el).html(this.template({location: this.model.getLocation()}));
+  }
+});
+
+var obiLocationView = new ObiWanLocation();
