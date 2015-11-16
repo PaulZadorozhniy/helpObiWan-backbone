5b9dadf0cab314b87618a58e72805c079056f26a
Paul Zadorozhniy <paulzadorozhniy@gmail.com>
2015-11-16 21:47:33 +0200

    created skeleton for darkLordsListView

---
+++ public/views/darkLordsListView.js
@@ -0,0 +1,24 @@
+
+var LordList = Backbone.View.extend({
+  el: $(".lord-list"),
+  template: _.template($('#lordList').html()),
+
+  initialize: function() {
+    //this.model = new ObiLocation();
+    //var self = this;
+    //var socket = new WebSocket('ws://jedi.smartjs.academy');
+    //
+    //socket.onmessage = function(event) {
+    //  self.model.setLocation(JSON.parse(event.data).name);
+    //  self.render();
+    //};
+
+    this.render();
+  },
+
+  render: function () {
+    $(this.el).html(this.template());
+  }
+});
+
+var lordListView = new LordList();
